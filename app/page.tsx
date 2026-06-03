254d246b8bcab65f75b87fec6c73daaed7644cb2
Dukens St Louis <lkgroupsainfo@gmail.com>
2026-06-03 16:18:39 -0400

    Amelioration accueil et popup connexion

--- app/page.tsx
+++ app/page.tsx
@@ -472,7 +472,7 @@ return (
 
       {/* MATCHS */}
 
-<div className="px-4 pb-10 pt-[250px] md:pt-[650px]">
+<div className="px-4 pb-10 pt-[650px] md:pt-[650px]">
 
   <div className="flex justify-center mb-8">
 
