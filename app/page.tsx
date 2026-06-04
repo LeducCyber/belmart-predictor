62c6936f8a1e08140d4e5ce961eba2a4ddbebe53
Dukens St Louis <lkgroupsainfo@gmail.com>
2026-06-04 11:02:11 -0400

    Adjust mobile match card spacing

--- app/page.tsx
+++ app/page.tsx
@@ -483,7 +483,7 @@ return (
   </a>
 </div>
 
-<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10">
+<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-12">
 
          {(showAllMatches ? matches : matches.slice(0, 6)).map(
   (match, index) => (
