4021f2384bd4e868a9905cf4291bef644cd7dd3e
Dukens St Louis <lkgroupsainfo@gmail.com>
2026-06-04 10:06:15 -0400

    Nouvelle image de fond et corrections responsive

--- app/page.tsx
+++ app/page.tsx
@@ -474,7 +474,7 @@ return (
 
 <div className="px-4 pb-10 pt-[20px] md:pt-[400px] lg:pt-[800px]">
 
-<div className="flex justify-center lg:justify-end mb-4 px-4">
+<div className="flex justify-end mb-2 px-4">
   <a
     href="/signup"
     className="w-full max-w-[260px] bg-yellow-400 text-[#082567] py-3 px-4 rounded-2xl font-black text-center text-base md:text-xl shadow-lg"
@@ -483,8 +483,7 @@ return (
   </a>
 </div>
 
-
-   <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
+<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10">
 
          {(showAllMatches ? matches : matches.slice(0, 6)).map(
   (match, index) => (
