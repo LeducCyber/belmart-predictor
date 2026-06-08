dac0169130aa365dbc35d8080567d4f1fee6ded4
Dukens St Louis <lkgroupsainfo@gmail.com>
2026-06-08 10:58:51 -0400

    Réduction taille bouton inscription mobile

--- app/page.tsx
+++ app/page.tsx
@@ -560,7 +560,7 @@ return (
   <div className="flex justify-center mb-8">
   <a
     href="/signup"
-    className="bg-yellow-400 hover:bg-yellow-500 text-[#082567] font-black text-lg px-8 py-4 rounded-2xl shadow-lg transition-all duration-300"
+  className="bg-yellow-400 hover:bg-yellow-500 text-[#082567] font-black text-sm md:text-lg px-4 md:px-8 py-2 md:py-4 rounded-xl md:rounded-2xl shadow-lg transition-all duration-300"
   >
     👉 Cliquez pour vous inscrire
   </a>
